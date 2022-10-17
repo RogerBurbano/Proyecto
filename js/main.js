0dc348eb621158b8f3b4d69a1f1280606a076e32
Roger Burbano <coyote2803@gmail.com>
2022-10-17 16:31:00 -0500

    proyect

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const btnRoger = document.querySelector("#roger");
+
+btnRoger.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  btnRoger.classList.toggle("active");
+});
+
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (e.currentTarget.nombre.value == "") {
+    alert("El campo de nombre esta vacio");
+    return;
+  }
+
+  if (e.currentTarget.contraseña.value == "") {
+    alert("Contraseña Requerida");
+    return;
+  }
+
+  alert(
+    `Bienvenido ${e.currentTarget.nombre.value}`
+  );
+});
+
